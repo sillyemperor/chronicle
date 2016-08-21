edf9ccb3616a7aa43a8540147f37dcb40614820e
peter <sillyemperor@qq.com>
2016-08-21 15:55:07 +0800

    merge event rout;

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def index():
         request.cookies['event-q'] = ''
 
     if 'q' not in request.query:
-        q = request.cookies['event-q'].decode('utf-8')
+        q = 'event-q' in request.cookies and request.cookies['event-q'].decode('utf-8') or ''
 
     p = int(request.query.p or 0)
     ps = int(request.query.ps or 10)
@@ -40,12 +40,18 @@ def index():
 
 
 @get('/event')
-def add_event():
-    return view.render('event.html', title='Event', event=models.Event())
+@get('/event/<id:int>')
+def event(id=0):
+    try:
+        event = session.query(models.Event).filter(models.Event.id == id).one()
+    except:
+        event = models.Event()
+    return view.render('event.html', title='Event', referer=request.headers.get('Referer'), event=event)
 
 
 @post('/event')
-def add_post_event():
+@post('/event/<id:int>')
+def post_event(id=0):
     title = request.forms.get('title')
     abstract = request.forms.get('abstract')
     year = request.forms.get('year')
@@ -56,49 +62,34 @@ def add_post_event():
     day2 = request.forms.get('day2')
     online_uri_list = request.forms.get('online_uri_list')
     thumbnail_uri_list = request.forms.get('thumbnail_uri_list')
-    session.add(models.Event(
-        title = unicode(title, 'utf-8'),
-        abstract = unicode(abstract, 'utf-8'),
-        year = year,
-        month = month,
-        day = day,
-        year2 = year2,
-        month2 = month2,
-        day2 = day2,
-        online_uri_list=online_uri_list,
-        thumbnail_uri_list = thumbnail_uri_list
-    ))
+
+    try:
+        event = session.query(models.Event).filter(models.Event.id == id).one()
+        event.abstract = unicode(abstract, 'utf-8')
+        event.title = unicode(title, 'utf-8')
+        event.year = year
+        event.month = month
+        event.day = day
+        event.year2 = year2
+        event.month2 = month2
+        event.day2 = day2
+        event.online_uri_list = online_uri_list
+        event.thumbnail_uri_list = thumbnail_uri_list
+    except:
+        session.add(models.Event(
+            title=unicode(title, 'utf-8'),
+            abstract=unicode(abstract, 'utf-8'),
+            year=year,
+            month=month,
+            day=day,
+            year2=year2,
+            month2=month2,
+            day2=day2,
+            online_uri_list=online_uri_list,
+            thumbnail_uri_list=thumbnail_uri_list
+        ))
     session.commit()
     redirect('/event')
-
-
-@get('/event/<id:int>')
-def edit_event(id):
-    return view.render('event.html', title='Event', event=session.query(models.Event).filter(models.Event.id == id).one())
-
-
-@post('/event/<id:int>')
-def edit_post_event(id):
-    title = request.forms.get('title')
-    abstract = request.forms.get('abstract')
-    year = request.forms.get('year')
-    month = request.forms.get('month')
-    day = request.forms.get('day')
-    year2 = request.forms.get('year2')
-    month2 = request.forms.get('month2')
-    day2 = request.forms.get('day2')
-    online_uri_list = request.forms.get('online_uri_list')
-    thumbnail_uri_list = request.forms.get('thumbnail_uri_list')
-
-    event = session.query(models.Event).filter(models.Event.id == id).one()
-    event.abstract = unicode(abstract, 'utf-8')
-    event.title = unicode(title, 'utf-8')
-    event.year = year
-    event.month = month
-    event.day = day
-    session.commit()
-
-    redirect('/')
 
 run(host='localhost', port=8080)
 
