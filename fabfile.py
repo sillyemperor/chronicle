3d78894d4b20008886503c4cb8c55b9d2f9cb518
wangjiang <wangj@dibugroup.cn>
2018-01-11 16:46:16 +0800

    CI测试

--- fabfile.py
+++ fabfile.py
@@ -1,1 +1,12 @@
 # -*- coding: utf-8 -*-
+from fabric.api import run
+from fabric.state import env
+
+env.user = 'root'
+env.password = 'Qwer3306Cd'
+
+def host_type():
+    run('uname -s')
+
+def pull():
+    run('git pull')
