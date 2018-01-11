1c147f25def6b424988556ad0ae94efcb7441c85
wangjiang <wangj@dibugroup.cn>
2018-01-11 20:30:54 +0800

    CI测试

--- fabfile.py
+++ fabfile.py
@@ -2,19 +2,17 @@
 from fabric.api import *
 from fabric.state import env
 
-env.project_root = 'chronicle/adminsite'
 
-def venv():
+def deploy():
     with cd('chronicle'):
+        run('git pull')
         run('source venv/bin/activate')
         run('python -m pip install -r requirements.txt')
-
-def deploy_static():
-    with cd(env.project_root):
-        run('python manage.py collectstatic -v0 --noinput')
-        run('python manage.py migrate')
-        run('pkill -9 gunicorn')
-        run('gunicorn -c gunisettings.py adminsite.wsgi & > /dev/null')
+        with cd('adminsite'):
+            run('python manage.py collectstatic -v0 --noinput')
+            run('python manage.py migrate')
+            run('pkill -9 gunicorn')
+            run('gunicorn -c gunisettings.py adminsite.wsgi & > /dev/null')
 
 
 
