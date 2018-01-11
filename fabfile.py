2db5ac5fe7661163f337629727c05d8a7e278b55
wangjiang <wangj@dibugroup.cn>
2018-01-11 20:28:33 +0800

    CI测试

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
-from fabric.api import run
+from fabric.api import *
 from fabric.state import env
 
-def host_type():
-    run('uname -s')
+env.project_root = 'chronicle/adminsite'
+
+def venv():
+    with cd('chronicle'):
+        run('source venv/bin/activate')
+        run('python -m pip install -r requirements.txt')
+
+def deploy_static():
+    with cd(env.project_root):
+        run('python manage.py collectstatic -v0 --noinput')
+        run('python manage.py migrate')
+        run('pkill -9 gunicorn')
+        run('gunicorn -c gunisettings.py adminsite.wsgi & > /dev/null')
+
+
+
