06ce21c0ebe207ff416acb6bb4417f1801ff6ca4
wangjiang <wangj@dibugroup.cn>
2018-01-11 20:40:37 +0800

    CI测试

--- fabfile.py
+++ fabfile.py
@@ -8,11 +8,11 @@ def deploy():
         run('git pull')
         run('source venv/bin/activate')
         run('python -m pip install -r requirements.txt')
-        with cd('adminsite'):
-            run('python manage.py collectstatic -v0 --noinput')
-            run('python manage.py migrate')
-            run('pkill -9 gunicorn')
-            run('gunicorn -c gunisettings.py adminsite.wsgi & > /dev/null')
+    with cd('chronicle/adminsite'):
+        run('python manage.py collectstatic -v0 --noinput')
+        run('python manage.py migrate')
+        run('pkill -9 gunicorn')
+        run('gunicorn -c gunisettings.py adminsite.wsgi & > /dev/null')
 
 
 
