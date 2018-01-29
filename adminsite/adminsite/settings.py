54d41e3f766df95bcdffa381f463944807802186
wangjiang <wangj@dibugroup.cn>
2018-01-29 23:53:44 +0800

    增加事件经纬度

--- adminsite/adminsite/settings.py
+++ adminsite/adminsite/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'zh_Hans'
+LANGUAGE_CODE = 'zh-hans'
 
 TIME_ZONE = 'UTC'
 
