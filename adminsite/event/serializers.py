cc7935bad01e8fcc46307d5a6f6939191f49b32a
Wang Jiang <sillyemperor@163.com>
2019-04-22 21:45:38 +0800

    -a
    界面：增加地图标注

--- adminsite/event/serializers.py
+++ adminsite/event/serializers.py
@@ -3,9 +3,15 @@ from rest_framework import serializers
 from models import Event, Tag
 
 class EventSerializer(serializers.ModelSerializer):
+    location = serializers.SerializerMethodField()
     class Meta:
         model = Event
-        fields  = ['year', 'month', 'day', 'year2', 'month2', 'day2', 'abstract', 'online_url']
+        fields  = ['year', 'month', 'day', 'year2', 'month2', 'day2', 'abstract', 'online_url', 'location']
+
+    def get_location(self, obj):
+        if obj.longitude and obj.latitude:
+            return [obj.longitude, obj.latitude]
+        return None
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
